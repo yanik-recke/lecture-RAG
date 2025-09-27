f708a7824db6a44862ccceae3f399ce07453514d
Yanik Recke <recke.yanik@outlook.de>
2025-09-27 21:37:19 +0200

    Added implementation for audio transcription service

---
+++ service_speechtotext/run.py
@@ -0,0 +1,50 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline, GenerationConfig
+from flask import Flask, request, jsonify
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "primeline/whisper-large-v3-turbo-german"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    processor=processor,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    device=device,
+    torch_dtype=torch_dtype,
+    return_timestamps=True
+)
+
+# Routes
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({'status':'healthy'})
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    if 'audio' not in request.files:
+        return jsonify({'error': 'No audio file provided'}), 400
+    
+    file = request.files['audio']
+
+    if file:
+        audio_data = file.read()
+
+        result = pipe(audio_data, generate_kwargs={'language': 'german'})
+        print(result)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8003, debug=True)
