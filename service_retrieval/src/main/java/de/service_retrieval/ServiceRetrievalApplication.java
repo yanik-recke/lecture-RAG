3db8d6d0b664f7beea89d4aae39fbc4a604ced1e
Yanik Recke <recke.yanik@outlook.de>
2025-09-28 20:01:57 +0200

    Initial commit for retrieval service

---
+++ service_retrieval/src/main/java/de/service_retrieval/ServiceRetrievalApplication.java
@@ -0,0 +1,13 @@
+package de.service_retrieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRetrievalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRetrievalApplication.class, args);
+    }
+
+}
