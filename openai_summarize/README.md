846f66a77f97c44c66119aa4cc060355d561184b
Yanik Recke <recke.yanik@outlook.de>
2025-11-11 12:38:14 +0100

    Added script to summarize the transcriptions of the lectures using the OpenAI API

--- openai_summarize/README.md
+++ openai_summarize/README.md
@@ -1,1 +1,8 @@
 # Using OpenAI's API to Create Summaries for the Transcribed Lectures
+
+Since the lectures are rather long, 
+the content may exceed the context window 
+of locally run models. If that is the
+case use this script as 
+a pivoting point to generate summaries for 
+the lectures.
--- openai_summarize/main.py
+++ openai_summarize/main.py
@@ -1,5 +1,29 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+# This script provides an easy option
+# to create summaries of the transcribed lectures.
+# These summaries could also be embedded or be
+# used to enhance the results of the
+# semantic search by combining it with keyword search.
+
+instructions = "Du sollst eine Vorlesung zum Thema Berechenbarkeit und Komplexität zusammenfassen. Deine Zusammenfassung ist später wichtig, um auf den ersten Blick zu erkennen, was in einer Vorlesung behandelt wurde, damit bei der Klausurvorbereitung direkt klar ist, welche Vorlesung man sich noch einmal anschauen sollte, wenn man ein bestimmtes Thema nicht verstanden hat. Achte besonders darauf, ob Angaben dazu gemacht werden, ob etwas in der Klausur drankommt. Solche Informationen sollten als [WICHTIG] markiert werden.\n\nFokussiere dich auf Schlagwörter und ignoriere Konversationen und Inhalte, die nicht zum Thema passen. Halte dich kurz aber bleib präzise. Stelle keine Rückfragen. Beende deine Zusammenfassung mit einer Liste an Schlagwörtern, die in einer Keyword Search verwendet werden können."
+
 def main():
-    print("Hello from openai-summarize!")
+    load_dotenv(".env")
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    with open("files/input_bk1", "r", encoding="utf-8") as f:
+        input_text = f.read()
+
+    result = client.responses.create(
+        model="gpt-5-mini",
+        instructions=instructions,
+        input=input_text,
+    )
+
+    print(result.output_text)
 
 
 if __name__ == "__main__":
