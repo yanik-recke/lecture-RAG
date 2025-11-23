ec8e11cb8e3bb37f9cafc99d17f0914390e70896
Yanik Recke <recke.yanik@outlook.de>
2025-11-23 21:15:37 +0100

    Updated path to Dockerfile in BuildFunctions.groovy

--- jenkins/BuildFunctions.groovy
+++ jenkins/BuildFunctions.groovy
@@ -21,7 +21,8 @@ def buildService(String serviceName, String serviceDir, String version) {
             buildctl build \
                 --frontend=dockerfile.v0 \
                 --local context=. \
-                --local dockerfile=. \
+                --local dockerfile= ./${serviceDir} \
+                --platform linux/amd64 \
                 --output type=image,name=${REPO}/${serviceName}:SNAPSHOT-${version},push=true
         """
     }
