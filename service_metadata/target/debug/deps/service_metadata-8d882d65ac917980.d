d1063948274550f1c2f9db173f4df44b5961b721
Yanik Recke <recke.yanik@outlook.de>
2025-12-09 09:37:43 +0100

    Added metadata service project

---
+++ service_metadata/target/debug/deps/service_metadata-8d882d65ac917980.d
@@ -0,0 +1,5 @@
+/Users/yanik/_repos/lecture-RAG/service_metadata/target/debug/deps/service_metadata-8d882d65ac917980.d: src/main.rs
+
+/Users/yanik/_repos/lecture-RAG/service_metadata/target/debug/deps/libservice_metadata-8d882d65ac917980.rmeta: src/main.rs
+
+src/main.rs:
---
+++ service_metadata/target/debug/deps/service_metadata-c97df65ca6e08581.d
@@ -0,0 +1,5 @@
+/Users/yanik/_repos/lecture-RAG/service_metadata/target/debug/deps/service_metadata-c97df65ca6e08581.d: src/main.rs
+
+/Users/yanik/_repos/lecture-RAG/service_metadata/target/debug/deps/libservice_metadata-c97df65ca6e08581.rmeta: src/main.rs
+
+src/main.rs:
